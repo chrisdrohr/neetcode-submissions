884601b6ba91278215441eab20154089a2ce2bf3
Chris Rohr <chrisdrohr@gmail.com>
2026-08-28 10:39:46 -0700

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        if (nums.length === 0) return 0;
+
+        const set = new Set(nums);
+        let result = 0;
+
+        for (const num of set) {
+            let count = 1;
+
+            if (!set.has(num - 1)) {
+                while (set.has(num + count)) {
+                    count++;
+                }
+            }
+
+            result = Math.max(result, count);
+        }
+
+        return result;
+    }
+}

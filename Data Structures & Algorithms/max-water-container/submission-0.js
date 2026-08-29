f7e67fb18f2b2f11b67ee884532b77cf04c707aa
Chris Rohr <chrisdrohr@gmail.com>
2026-08-29 09:55:13 -0700

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let maxArea = 0;
+        let left = 0;
+        let right = heights.length - 1;
+
+        while (left <= right) {
+            const area = (right - left) * Math.min(heights[left], heights[right]);
+            maxArea = Math.max(area, maxArea)
+            if (heights[left] < heights[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
